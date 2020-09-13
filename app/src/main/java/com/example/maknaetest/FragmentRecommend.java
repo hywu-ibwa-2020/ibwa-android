2ace165570bf9faac91376a16362364805c10d64
kanghw1103 <kanghw1103@naver.com>
2020-09-13 23:26:37 +0900

    change fragment_recommend.xml

--- app/src/main/java/com/example/maknaetest/FragmentRecommend.java
+++ app/src/main/java/com/example/maknaetest/FragmentRecommend.java
@@ -1,9 +1,11 @@
 package com.example.maknaetest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,11 +28,24 @@ public class FragmentRecommend extends Fragment {
         lstCard.add(new Card("기분전환이 필요해","category card","description card",R.drawable.first));
         lstCard.add(new Card("LOVE HOUSE","category card","description card",R.drawable.second));
         lstCard.add(new Card("위로받고 싶은 날","category card","description card",R.drawable.third));
+        lstCard.add(new Card("스터디집중모드","category card","description card",R.drawable.card_book));
+        lstCard.add(new Card("집순이를 위한","category card","description card",R.drawable.card_home));
 
         RecyclerView myrv = (RecyclerView) recommended_view.findViewById(R.id.recycler_id);
         RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(lstCard, getActivity());
         myrv.setLayoutManager(new GridLayoutManager(getActivity(),2));
         myrv.setAdapter(myAdapter);
+
+//        // 막내의 pick 이용하기 버튼
+//        Button pick_btn = (Button) recommended_view.findViewById(R.id.pick_btn);
+//        // 버튼 누르면 activity_card 액티비티(뮤직화면)로 이동.
+//        pick_btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent pick_intent = new Intent(getActivity(), Card_Activity.class);
+//                startActivity(pick_intent);
+//            }
+//        });
 
         return recommended_view;
     }
