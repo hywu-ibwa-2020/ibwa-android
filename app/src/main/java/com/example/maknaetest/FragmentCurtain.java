507a17cf9409bde53e284993ccab90eb279f963c
kanghw1103 <kanghw1103@naver.com>
2020-10-07 01:27:57 +0900

    조도갑 바뀌면 메인 조도상태이미지 변경

--- app/src/main/java/com/example/maknaetest/FragmentCurtain.java
+++ app/src/main/java/com/example/maknaetest/FragmentCurtain.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,6 +23,16 @@ public class FragmentCurtain extends Fragment {
     ImageButton lightPower_btn;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View curtain_view =  inflater.inflate(R.layout.fragment_curtain, container, false);
+
+        int sun_state = 100; // 조도값으로 받아오는 값 저장
+        ImageView lighting = (ImageView) curtain_view.findViewById(R.id.lighting);
+
+        if (sun_state > 100 && sun_state < 300) //조도값이 100~300이면 좋음 값 출력
+            lighting.setImageResource(R.drawable.lighting_blue);
+        else if (sun_state > 301 && sun_state < 500) //조도값이 301~500이면 보통 값 출력
+            lighting.setImageResource(R.drawable.lighting_green);
+        else //조도값이 500초과이면 나쁨 값 출력
+            lighting.setImageResource(R.drawable.lighting_red);
 
 //        lightPower_btn.setOnClickListener(new OnClickListener() {
 //            @Override
